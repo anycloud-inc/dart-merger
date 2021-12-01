fcd974d6b18b0573fd430da0d8c77fdde5b560a8
muraikenta <muraikenta0507@gmail.com>
2021-12-01 23:51:58 +0900

    proto

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
+import glob = require("glob");
+import fs = require("fs");
+
 module.exports = () => {
   const [, , firstArg] = process.argv;
 
-  if (!firstArg) {
-    console.error("Please pass one argument!!");
-    process.exit(1);
+  const packageName = "example";
+  const lib = process.cwd() + "/example/lib";
+  const out = process.cwd() + "/example/out/out.dart";
+
+  const files = glob.sync(lib + "/**/*.dart");
+  console.log(files);
+
+  let imports: string[] = [];
+  let buffer = "";
+
+  for (const file of files) {
+    let text = fs.readFileSync(file, "utf-8");
+    text = text.replace(/^part .*\n$/m, "");
+    text = text.replace(/^import .*\n$/m, (str) => {
+      if (str.match(new RegExp(`package:${packageName}`)) == null) {
+        // 外部packageであれば追加
+        imports.push(str);
+      }
+      return "";
+    });
+    buffer += text + "\n";
   }
 
-  const msg = `
-  Hello!! ${firstArg} san.
-  I am Toshihisa Tomatsu.
-  GitHub: https://github.com/toshi-toma
-  Twitter: https://twitter.com/toshi__toma
-`;
+  buffer = imports.join("\n") + "\n" + buffer;
 
-  console.log(msg);
+  fs.writeFileSync(out, buffer);
+
+  console.log("imports");
+  console.log(imports);
+
+  console.log("successfully merged 🎉");
 };
