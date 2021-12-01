aad045edd858c32aba504549c9bce65427b4df5f
muraikenta <muraikenta0507@gmail.com>
2021-12-02 01:04:09 +0900

    packageNameを必須に

--- src/cli.ts
+++ src/cli.ts
@@ -3,21 +3,24 @@ import cac from "cac";
 import merge from "./merge";
 
 const cli = cac();
-cli.option("--package <packageName>", "Provide package name", {
-  default: "example",
-});
+cli.option("--package <packageName>", "Provide package name");
 cli.option("--src <folderPath>", "Provide source directory", { default: "." });
 cli.option("--out <filePath>", "Provide file path of output", {
   default: "out.dart",
 });
 
 cli.help();
-cli.showHelpOnExit = true;
 
 const parsed = cli.parse();
 
 if (parsed.options.help) {
   process.exit(0);
+}
+
+if (parsed.options.package == null) {
+  console.log("Error: --package parameter required\n");
+  cli.outputHelp();
+  process.exit(1);
 }
 
 merge({
